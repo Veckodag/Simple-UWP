7430b59baba47a7e738217cfe5669c4b2dde8647
Fredrik Sandström <Fredrik.Sandstrom@triona.se>
2017-01-25 16:33:48 +0100

    My NotePad. Simple working UWP project.

---
+++ MyNotepad/Services/FileServices.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using MyNotepad.Models;
+
+namespace MyNotepad.Services
+{
+  public class FileServices
+  {
+    public async Task SaveAsync(FileInfo model)
+    {
+      if (model != null)
+        await FileIO.WriteTextAsync(model.Ref, model.Text);
+    }
+
+    public async Task<Models.FileInfo> LoadAsync(Windows.Storage.StorageFile file)
+    {
+      return new FileInfo
+      {
+        Text = await FileIO.ReadTextAsync(file),
+        Name = file.DisplayName,
+        Ref = file
+      };
+
+    }
+  }
+}
---
+++ MyNotepad/Viewmodels/MainPageViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage.Pickers;
+using MyNotepad.Models;
+using MyNotepad.Services;
+
+namespace MyNotepad.Viewmodels
+{
+  public class MainPageViewModel : INotifyPropertyChanged
+  {
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private FileInfo _file;
+
+    public FileInfo File
+    {
+      get { return _file; }
+      set
+      {
+        _file = value;
+       PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(File)));
+      }
+    }
+
+    FileServices _fileServices = new FileServices();
+
+    public async void Save()
+    {
+      await _fileServices.SaveAsync(File);
+    }
+
+    public async void Open()
+    {
+
+      var picker = new FileOpenPicker
+      {
+        ViewMode = PickerViewMode.List,
+        SuggestedStartLocation = PickerLocationId.DocumentsLibrary
+      };
+
+      picker.FileTypeFilter.Add(".txt");
+
+      var file = await picker.PickSingleFileAsync();
+
+      if (file == null)
+        await new Windows.UI.Popups.MessageDialog("No file selected.").ShowAsync();
+      else
+        File = await _fileServices.LoadAsync(file);
+    }
+  }
+}
